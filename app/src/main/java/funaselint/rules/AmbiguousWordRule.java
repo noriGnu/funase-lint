ed8ad8c76624ac694f465d6f7e05b482334c6589
noriGnu <kento141121@icloud.com>
2023-11-20 13:22:22 +0900

    "?"にも対応できるようにしました

--- app/src/main/java/funaselint/rules/AmbiguousWordRule.java
+++ app/src/main/java/funaselint/rules/AmbiguousWordRule.java
@@ -10,8 +10,7 @@ import org.w3c.dom.NodeList;
 
 public class AmbiguousWordRule extends Rule {
 
-    // 警告メッセージを出力する曖昧な言葉を設定
-    private static final List<String> ambiguousWords = Arrays.asList("かなり", "非常に", "とても");
+    private static final List<String> ambiguousWords = Arrays.asList("かなり", "非常に");
 
     @Override
     public List<String> applicableFilesOrFolders() {
@@ -25,10 +24,18 @@ public class AmbiguousWordRule extends Rule {
             Node textNode = textNodes.item(i);
             String textContent = textNode.getTextContent();
 
+            // "?"が含まれているかどうかを確認
+            if (textContent.contains("?")) {
+                System.out.println("Warning: \"?\"が使用されています。 File: " + file.getPath() + ", Text: " + textContent);
+                System.out.println("?とか使うのやめようよ。子どもの文章じゃあるまいし。");
+                return true;
+            }
+
             // 曖昧な言葉が含まれているかどうかを確認
             for (String ambiguousWord : ambiguousWords) {
                 if (textContent.contains(ambiguousWord)) {
-                    System.out.println("Warning: 主観的評価は取り除きなさいな。 File: " + file.getPath() + ", Text: " + textContent);
+                    System.out.println("Warning: 曖昧な言葉が使用されています。 File: " + file.getPath() + ", Text: " + textContent);
+                    System.out.println("可能な限り主観的な評価を取り除くことが工学の文章の考え方です。");
                     return true;
                 }
             }
